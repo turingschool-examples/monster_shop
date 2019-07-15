8e6f7d79ec3bbad531c0cbfa2738b3f1d2aae386
Jake Miller <jake0miller@gmail.com>
2019-07-15 11:36:50 -0600

    Add state to user table

--- db/migrate/20190715173521_create_users.rb
+++ db/migrate/20190715173521_create_users.rb
@@ -4,6 +4,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :address
       t.string :city
+      t.string :state
       t.integer :zip
       t.string :email
       t.string :password_digest
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190715173308) do
+ActiveRecord::Schema.define(version: 20190715173521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20190715173308) do
     t.string "name"
     t.string "address"
     t.string "city"
+    t.string "state"
     t.integer "zip"
     t.string "email"
     t.string "password_digest"
