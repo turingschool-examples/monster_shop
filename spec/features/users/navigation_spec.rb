1e8e214fb9bc7ffd597310b8537ea1e260225c29
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-15 19:36:45 -0600

    Add test for logged in as "users name" within navbar

--- spec/features/users/navigation_spec.rb
+++ spec/features/users/navigation_spec.rb
@@ -37,5 +37,26 @@ RSpec.describe 'User Navigation' do
         expect(page).to_not have_link('Logout')
       end
     end
+
+    it 'I can see a welcome message in navbar' do
+      user = User.create(name: "Test Test", address: "123", city: "Denver", state: "CO", zip: 80209, email: "test@gmail.com", password: "password", role: 0)
+      visit root_path
+
+      within 'nav' do
+        click_on 'Login'
+      end
+
+      expect(current_path).to eq(login_path)
+
+      within '#login' do
+        fill_in "Email", with: user.email
+        fill_in "Password", with: user.password
+        click_on 'Login'
+      end
+
+      within 'nav' do
+        expect(page).to have_content("Logged in as #{user.name}")
+      end
+    end
   end
 end
