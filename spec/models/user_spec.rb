62c0856a25458788d72ca8839c4cc465758ebae6
Martha Troubh <indexer@icloud.com>
2019-07-16 17:20:03 -0600

    Add tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,37 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 describe User, type: :model do
-  describe "validations" do
-    it {should validate_presence_of(:name)}
-    it {should validate_presence_of(:address)}
-    it {should validate_presence_of(:city)}
-    it {should validate_presence_of(:state)}
-    it {should validate_length_of(:state)}
-    it {should validate_presence_of(:zip)}
-    it {should validate_length_of(:zip)}
-    it {should validate_numericality_of(:zip)}
-    it {should validate_presence_of(:email)}
-    it {should validate_uniqueness_of(:email)}
-    it {should validate_presence_of(:password)}
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:address) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_length_of(:state) }
+    it { should validate_presence_of(:zip) }
+    it { should validate_length_of(:zip) }
+    it { should validate_numericality_of(:zip) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:password) }
+  end
+
+  describe User do
+    describe 'roles' do
+      it 'can be created as an admin' do
+        user = User.create(email: 'penelope', password: 'boom', role: 1)
+
+        expect(user.role).to eq('admin')
+        expect(user.admin?).to be_truthy
+      end
+
+      it 'can be created as a default user' do
+        user = User.create(email: 'sammy', password: 'pass')
+
+        expect(user.role).to eq('default')
+        expect(user.default?).to be_truthy
+      end
+    end
   end
 end
