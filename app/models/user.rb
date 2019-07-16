f3e064ac57d63d1d74b7c6bc035a00417e69e0d8
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-16 15:17:30 -0600

    Add enum roles to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
+
   validates :email, uniqueness: true, presence: true
   validates :state, presence: true, length: { is: 2 }
   validates :zip, presence: true, numericality: true, length: { is: 5 }
   validates_presence_of :name, :address, :city
+
+  enum role: ["regular_user", "merchant", "admin"]
 end
