f8accb487309f863a9ac1eef831b3ce5ac2d61a4
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-15 19:24:32 -0600

    Fix user creation and add within

--- spec/features/users/navigation_spec.rb
+++ spec/features/users/navigation_spec.rb
@@ -4,12 +4,28 @@ RSpec.describe 'User Navigation' do
   describe 'As a User' do
 
     it 'I can not see a link to log in or register when I am logged in' do
-      user = User.create(name: "Test Test", address: "123", city: "Denver", state: "CO", zip: 80209, email: "test@gmail.com", password: "password", role: 1)
+      user = User.create(name: "Test Test", address: "123", city: "Denver", state: "CO", zip: 80209, email: "test@gmail.com", password: "password", role: 0)
       visit root_path
 
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-      expect(page).to_not have_button('Register')
-      expect(page).to_not have_button('Login')
+      within 'nav' do
+        click_on 'Login'
+      end
+
+      expect(current_path).to eq(login_path)
+
+      within '#login' do
+        fill_in "Email", with: user.email
+        fill_in "Password", with: user.password
+        click_on 'Login'
+      end
+
+      expect(current_path).to eq(root_path)
+
+      within 'nav' do
+        expect(page).to_not have_link('Register')
+        expect(page).to_not have_link('Login')
+      end
+
     end
   end
 end
