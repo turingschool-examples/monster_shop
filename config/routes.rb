c3ce8bd4d904c7b47cd6aa05dbec81d8b2cf09fb
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-16 15:42:46 -0600

    Add merchant dashboard path to routes

--- config/routes.rb
+++ config/routes.rb
@@ -25,11 +25,14 @@ Rails.application.routes.draw do
 
   post '/users', to: 'users#create', as: :users
   get '/users/:id', to: 'users#show', as: :user
-  get '/users/:id/edit', to: 'users#edit', as: :edit_user 
+  get '/users/:id/edit', to: 'users#edit', as: :edit_user
   patch '/users/:id', to: 'users#update', as: :update_user
 
   get '/profile', to: 'users#show', as: :profile
+  get '/merchant', to: 'merchant/dashboard#show', as: :merchant_dashboard
+
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+
 end
