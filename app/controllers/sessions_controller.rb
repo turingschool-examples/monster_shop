9cb2e954c15ac1acd62cd4e517250a8182131282
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-16 15:13:21 -0600

    Change redirect path to profile path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_path
+      redirect_to profile_path
     else
       render :new
     end
