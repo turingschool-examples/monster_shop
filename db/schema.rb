4fec77a4e5d8af1383d38d95739b629d55f55755
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-15 19:25:13 -0600

    Add role to Users db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190715173521) do
+ActiveRecord::Schema.define(version: 20190715215927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 20190715173521) do
     t.integer "zip"
     t.string "email"
     t.string "password_digest"
+    t.integer "role", default: 0
   end
 
   add_foreign_key "items", "merchants"
