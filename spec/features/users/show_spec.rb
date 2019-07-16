296db533997ef40d6277c2dff495a39f9dfe4c2d
Martha Troubh <indexer@icloud.com>
2019-07-16 17:18:26 -0600

    Add tests for user roles

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -19,3 +19,51 @@ RSpec.describe 'Visitor' do
     end
   end
 end
+
+RSpec.describe 'As a registered user' do
+  describe 'when I visit my  profile page' do
+    before do
+      @user = User.create!(email: 'gijoe@icloud.com', password: 'combat', role: 0, name: 'Joe Camo', address: '543 Bootcamp Way', city: 'Fort Bragg', state: 'NC', zip: 28303)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+    end
+
+    it 'loads a page' do
+      visit profile_path
+
+      expect(current_path).to eq(profile_path)
+    end
+
+    describe 'shows any orders placed' do
+      it "has link 'My Orders'" do
+        visit profile_path
+
+        expect(page).to have_link('My Orders')
+      end
+
+      it "When I click 'My Orders' link, I am taken to /profile/orders/" do
+        visit profile_path
+
+        click_link('My Orders')
+
+        expect(current_path).to eq(profile_orders_path)
+      end
+    end
+  end
+end
+
+RSpec.describe 'As an unregistered user' do
+  it 'shows a 404 page if invalid user_id is entered' do
+    visit profile_path
+
+    expect(current_path).to eq(profile_path)
+    expect(page.status_code).to eq(404)
+    expect(page).to_not have_content('Name: ')
+    expect(page).to_not have_content('E-Mail: ')
+    expect(page).to_not have_content('Address: ')
+    expect(page).to_not have_content('City: ')
+    expect(page).to_not have_content('State: ')
+    expect(page).to_not have_content('Zip: ')
+    expect(page).to have_content("The page you were looking for doesn't exist")
+  end
+end
