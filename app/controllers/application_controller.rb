c68286e64ce261e51ab632c7d8290228bd71f60e
Martha Troubh <indexer@icloud.com>
2019-07-16 17:39:59 -0600

    Current user and current admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,12 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def current_user?
+  current_user && current_user.user?
+end
+
+  def current_admin?
+    current_user && current_user.admin?
+  end
 end
