345fa5a56242726633f38c29dbeb4c6ff501db88
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-17 12:18:42 -0600

    Add test to see merchant dashboard when logged in as a merchant

--- spec/features/users/navigation_spec.rb
+++ spec/features/users/navigation_spec.rb
@@ -96,5 +96,20 @@ RSpec.describe 'User Navigation' do
 
       expect(current_path).to eq(admin_dashboard_path)
     end
+
+    it "I see the same links as a visitor plus merchant dashboard and logout" do
+      @merchant = User.create(name: "Merchant", address: "456 Market St", city: "Denver", state: "CO", zip: 80210, email: "merchant@gmail.com", password: "taco_sauce", role: 1)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant)
+
+      visit root_path
+
+      within 'nav' do
+        expect(page).to have_content("Dashboard")
+        expect(page).to have_content("Logout")
+
+        expect(page).to_not have_content("Login")
+        expect(page).to_not have_content("Register")
+      end
+    end
   end
 end
