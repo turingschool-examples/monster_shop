04f286b32ced8ea5f16404fd01190c05217ad867
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-17 11:19:13 -0600

    Add destroy action to route and sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -23,4 +23,12 @@ class SessionsController < ApplicationController
       redirect_to login_path
     end
   end
+
+  def destroy
+    user = User.find_by(email: session[:email])
+    session[:user_id] = nil
+    redirect_to root_path
+    flash[:notice] = "You have logged out."
+  end
+
 end
