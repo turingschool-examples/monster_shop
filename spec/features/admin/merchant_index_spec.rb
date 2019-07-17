70af692a170594af54fe2bf68e3a7783dbb8f2ac
Sarah Tatro <statrox4@gmail.com>
2019-07-17 15:56:20 -0600

    Create test for admin accessing merchant show page

--- spec/features/admin/merchant_index_spec.rb
+++ spec/features/admin/merchant_index_spec.rb
@@ -2,12 +2,46 @@ require 'rails_helper'
 
 RSpec.describe 'Admin' do
   describe 'go to merchant index page click on merchant name' do
+    before :each do
+      @megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @sal = Merchant.create!(name: 'Sals Salamanders', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20.25, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
+      @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+      @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+
+      @meg = User.create!(name: 'Megan M', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'meg@gmail.com', password: 'fish' )
+      @meg2 = User.create!(name: 'Megan M', address: '123 Main St', city: 'Denver', state: 'IA', zip: 80218, email: 'meg2@gmail.com', password: 'fish' )
+      @order_1 = @meg.orders.create!
+      @order_2 = @meg2.orders.create!
+
+      @order_1.order_items.create!(item: @ogre, price: @ogre.price, quantity: 2)
+      @order_2.order_items.create!(item: @giant, price: @hippo.price, quantity: 2)
+      @order_2.order_items.create!(item: @ogre, price: @hippo.price, quantity: 2)
+    end
+
     it 'I can see everything a merchant would' do
-      
+      visit "/merchants/#{@megan.id}"
+
+      expect(page).to have_content(@megan.name)
+
+      within '.address' do
+        expect(page).to have_content(@megan.address)
+        expect(page).to have_content("#{@megan.city} #{@megan.state} #{@megan.zip}")
+      end
     end
 
     it 'my path is /admin/merchants/:merchant_id'do
+      visit "/admin/merchants/#{@megan.id}"
 
+      expect(page).to have_content(@megan.name)
+
+      within '.address' do
+        expect(page).to have_content(@megan.address)
+        expect(page).to have_content("#{@megan.city} #{@megan.state} #{@megan.zip}")
+      end
+
+      expect(current_path).to eq("/admin/merchants/#{@megan.id}")
     end
   end
 end
