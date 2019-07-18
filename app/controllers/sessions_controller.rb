2e40dcdc4915c84affdc042762aa92828677482a
Jake Miller <jake0miller@gmail.com>
2019-07-17 19:49:02 -0600

    Allow employee access to profile

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,9 @@ class SessionsController < ApplicationController
   def new
     unless current_user.nil?
       flash[:message] = 'You are already logged in.'
-      redirect_to profile_path if current_user.user?
-      redirect_to merchant_dashboard_path if current_user.merchant?
-      redirect_to admin_dashboard_path if current_user.admin?
+      redirect_to profile_path if current_user?
+      redirect_to merchant_dashboard_path if current_merchant? || current_employee?
+      redirect_to admin_dashboard_path if current_admin?
     end
   end
 
@@ -12,9 +12,9 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to profile_path if user.user?
-      redirect_to merchant_dashboard_path if current_user.merchant?
-      redirect_to admin_dashboard_path if current_user.admin?
+      redirect_to profile_path if current_user?
+      redirect_to merchant_dashboard_path if current_merchant? || current_employee?
+      redirect_to admin_dashboard_path if current_admin?
       flash[:success] = 'You have logged in.'
     else
       flash[:error] = 'Username and password do not match.'
