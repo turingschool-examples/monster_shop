7284ab3d39ae4f4276ec2eb9338e117d1b229307
Sarah Tatro <statrox4@gmail.com>
2019-07-18 13:41:25 -0600

    Set up admin merchant controller

---
+++ app/controllers/admin/merchants_controller.rb
@@ -0,0 +1,8 @@
+class Admin::MerchantsController < Admin::BaseController
+
+  def index
+    @merchants = Merchant.all
+  end
+
+  
+end
--- spec/features/admin/merchant_index_spec.rb
+++ spec/features/admin/merchant_index_spec.rb
@@ -5,19 +5,19 @@ RSpec.describe 'Admin' do
     before :each do
       @megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
       @brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
-      @larry = User.create!(name: "Larry Green", address: "345 Blue Lane", city: "Blue City", state: "CA", zip: 56789, email: "green@gmail.com", password: "frogs", role: 2)
+      @larry = User.create!(name: "Larry Green", address: "345 Blue Lane", city: "Blue City", state: "CA", zip: 56789, email: "green@gmail.com", password: "frogs", role: "admin")
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@larry)
-      visit 'admin_merchant_index'
+      visit admin_merchant_index_path 
     end
 
     it "The merchant's name is a link to their merchant dashboard" do
-      click_button "Megan's Marmalades"
+      click_link "Megan's Marmalades"
 
-      expect(current_path).to eq(merchant_dashboard_path)
+      expect(current_path).to eq(admin_merchant_show)
     end
 
     it "I see a disable button next to a merchant who is enabled" do
-      
+
       expect(page).to have_button('enabled')
     end
 
