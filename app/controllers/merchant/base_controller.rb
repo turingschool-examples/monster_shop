5b0d13d66990d34a39699d838be3972002f2506f
Jake Miller <jake0miller@gmail.com>
2019-07-17 19:46:53 -0600

    Allow employee to access merchant dashboard

--- app/controllers/merchant/base_controller.rb
+++ app/controllers/merchant/base_controller.rb
@@ -4,7 +4,7 @@ class Merchant::BaseController < ApplicationController
   private
 
   def require_merchant
-    unless current_merchant?
+    unless current_merchant? || current_employee?
       render file: '/public/404', status: 404, layout: false
     end
   end
