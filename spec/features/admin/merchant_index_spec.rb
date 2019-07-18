0f900048fe4ff1a34d82d98d560e259a5b48c400
Jake Miller <jake0miller@gmail.com>
2019-07-17 19:15:04 -0600

    Add extra tests

--- spec/features/admin/merchant_index_spec.rb
+++ spec/features/admin/merchant_index_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe 'Admin' do
     before :each do
       @megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
       @brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
-      @sal = Merchant.create!(name: 'Sals Salamanders', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
       @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20.25, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
       @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
       @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
@@ -21,12 +20,11 @@ RSpec.describe 'Admin' do
       @order_2.order_items.create!(item: @giant, price: @hippo.price, quantity: 2)
       @order_2.order_items.create!(item: @ogre, price: @hippo.price, quantity: 2)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@larry)
+      visit admin_merchant_show_path(@megan)
     end
 
     describe 'I can see everything a merchant would' do
-      it 'my path is /admin/merchants/:merchant_id'do
-        visit admin_merchant_show_path(@megan)
-
+      it 'my path is /admin/merchants/:merchant_id' do
         expect(page).to have_content(@megan.name)
 
         within '.address' do
@@ -35,6 +33,21 @@ RSpec.describe 'Admin' do
         end
 
         expect(current_path).to eq(admin_merchant_show_path(@megan))
+      end
+
+      it 'I see merchant statistics' do
+        within '.statistics' do
+          expect(page).to have_content("Item Count: #{@megan.item_count}")
+          expect(page).to have_content("Average Item Price: #{number_to_currency(@megan.average_item_price)}")
+          expect(page).to have_content("Cities Served:\nDenver, CO\nDenver, IA")
+        end
+      end
+
+      it 'I see stats for merchants with items, but no orders' do
+        within '.statistics' do
+          expect(page).to have_content("Item Count: #{@megan.item_count}")
+          expect(page).to have_content("Average Item Price: #{number_to_currency(@megan.average_item_price)}")
+        end
       end
     end
   end
