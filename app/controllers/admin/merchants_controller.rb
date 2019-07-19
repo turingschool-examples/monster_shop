16f221283eaed2c7483802dccfede1ba98c74365
Sarah Tatro <statrox4@gmail.com>
2019-07-19 14:21:37 -0600

    Add flash message to enable merchant

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -9,17 +9,19 @@ class Admin::MerchantsController < Admin::BaseController
   end
 
   def enable
-    @merchant = Merchant.find(params[:id])
-    @merchant.update(enabled: true)
+    merchant = Merchant.find(params[:id])
+    merchant.update(enabled: true)
+    flash[:notice] = "The account for #{merchant.name} is now enabled"
 
-    redirect_to admin_merchant_show_path(@merchant)
+    redirect_to admin_merchant_index_path
   end
 
   def disable
-    @merchant = Merchant.find(params[:id])
-    @merchant.update(enabled: false)
+    merchant = Merchant.find(params[:id])
+    merchant.update(enabled: false)
+    flash[:notice] = "The account for #{merchant.name} is now disabled"
 
-    redirect_to admin_merchant_show_path(@merchant)
+    redirect_to admin_merchant_index_path
   end
 
 end
