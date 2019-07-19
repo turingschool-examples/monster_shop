98facdcb221d814ff93e7009cd4d8fc478329f0c
Sarah Tatro <statrox4@gmail.com>
2019-07-18 20:01:30 -0600

    Update user role in before action to "admin" to reflect role changes in database -all tests pass

--- spec/features/admin/merchant_index_spec.rb
+++ spec/features/admin/merchant_index_spec.rb
@@ -4,14 +4,12 @@ RSpec.describe 'Admin' do
   describe "I visit the merchant's index page and see their city and state" do
     before :each do
       @megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, enabled: true)
-      @brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218, enabled: true)
       @larry = User.create!(name: "Larry Green", address: "345 Blue Lane", city: "Blue City", state: "CA", zip: 56789, email: "green@gmail.com", password: "frogs", role: "admin")
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@larry)
       visit admin_merchant_index_path
     end
 
     it "I see merchant name, city, and state displayed on the page" do
-
       expect(page).to have_link("Megans Marmalades")
       expect(page).to have_content(@megan.city)
       expect(page).to have_content(@megan.state)
@@ -23,25 +21,16 @@ RSpec.describe 'Admin' do
       expect(current_path).to eq(admin_merchant_show_path(@megan.id))
     end
 
-    it "A merchant is enabled by default" do
+    it "I can toggle a button to enable or disable a merchant" do
       visit admin_merchant_show_path(@megan.id)
       click_button 'Disable Merchant'
 
+      expect(page).to have_button('Enable Merchant')
+
+      visit admin_merchant_show_path(@megan.id)
+      click_button 'Enable Merchant'
+
       expect(page).to have_button('Disable Merchant')
-
-      # save_and_open_page
-
-      # within('#enabled') { expect(page).to have_content(true) }
     end
-
-
-    # it "I see a disable button next to a merchant who is enabled" do
-    #
-    # end
-    #
-    # it "I see an enable button next to a merchant who is disabled" do
-    #
-    #   expect(page).to have_button('enable')
-    # end
   end
 end
