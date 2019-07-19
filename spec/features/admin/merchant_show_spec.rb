24726cfec7b8b0845f763bb1bef871f9effd06bb
Sarah Tatro <statrox4@gmail.com>
2019-07-19 14:21:57 -0600

    Add test for flash message that merchant is now enabled

--- spec/features/admin/merchant_show_spec.rb
+++ spec/features/admin/merchant_show_spec.rb
@@ -23,12 +23,14 @@ RSpec.describe 'Admin' do
       visit admin_merchant_show_path(@megan.id)
       click_button 'Disable Merchant'
 
-      expect(page).to have_button('Enable Merchant')
+      expect(page).to have_content("The account for #{@megan.name} is now disabled")
+      expect(current_path).to eq(admin_merchant_index_path)
 
       visit admin_merchant_show_path(@megan.id)
       click_button 'Enable Merchant'
 
-      expect(page).to have_button('Disable Merchant')
+      expect(page).to have_content("The account for #{@megan.name} is now enabled")
+      expect(current_path).to eq(admin_merchant_index_path)
     end
   end
 end
