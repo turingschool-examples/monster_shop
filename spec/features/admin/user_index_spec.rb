7255392eb22903922425d2167a66d7327febc3f8
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-20 17:57:04 -0600

    Add expectation to find two more users and their info

--- spec/features/admin/user_index_spec.rb
+++ spec/features/admin/user_index_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 RSpec.describe 'Admin User Index Page' do
   describe "As an Admin" do
     before :each do
+      @megan = Merchant.create(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
       @larry = User.create(name: "Larry Green", address: "345 Blue Lane", city: "Blue City", state: "CA", zip: 56789, email: "green@gmail.com", password: "frogs", role: "admin")
-      @user = User.create(name: "User", address: "123 Clarkson St", city: "Denver", state: "CO", zip: 80209, email: "user@gmail.com", password: "cheetos", role: "user")
-      @employee = User.create(name: "Merchant", address: "456 Market St", city: "Denver", state: "CO", zip: 80210, email: "employee@gmail.com", password: "taco_sauce", role: "employee")
-      @merchant = User.create(name: "Merchant", address: "456 Market St", city: "Denver", state: "CO", zip: 80210, email: "merchant@gmail.com", password: "taco_sauce", role: "merchant")
+      @user = User.create(name: "Tyler", address: "123 Clarkson St", city: "Denver", state: "CO", zip: 80209, email: "user@gmail.com", password: "cheetos", role: "user")
+      @employee = User.create(name: "Merchant", address: "456 Market St", city: "Denver", state: "CO", zip: 80210, email: "employee@gmail.com", password: "taco_sauce", role: "employee", merchant_id: @megan.id)
+      @merchant_admin = User.create(name: "Merchant Admin", address: "456 Market St", city: "Denver", state: "CO", zip: 80210, email: "merchant@gmail.com", password: "taco_sauce", role: "merchant", merchant_id: @megan.id)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@larry)
     end
 
@@ -22,17 +23,22 @@ RSpec.describe 'Admin User Index Page' do
       within "#user-#{@user.id}" do
         expect(page).to have_link(@user.name, href: admin_user_show_path(@user.id))
         expect(page).to have_content(@user.created_at)
-        expect(page).to have_content(@user.role)
+        expect(page).to have_content(@user.role.capitalize)
+      end
+
+      within "#user-#{@employee.id}" do
+        expect(page).to have_link(@employee.name, href: admin_user_show_path(@employee.id))
+        expect(page).to have_content(@employee.created_at)
+        expect(page).to have_content(@employee.role.capitalize)
+      end
+
+      within "#user-#{@merchant_admin.id}" do
+        expect(page).to have_link(@merchant_admin.name, href: admin_user_show_path(@merchant_admin.id))
+        expect(page).to have_content(@merchant_admin.created_at)
+        expect(page).to have_content(@merchant_admin.role.capitalize)
       end
     end
 
+
   end
 end
-# As an admin user
-# When I click the "Users" link in the nav (only visible to admins)
-# Then my current URI route is "/admin/users"
-# Only admin users can reach this path.
-# I see all users in the system
-# Each user's name is a link to a show page for that user ("/admin/users/5")
-# Next to each user's name is the date they registered
-# Next to each user's name I see what type of user they are
