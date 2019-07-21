1ab8b9e63c2584f293fbae0303d2ed53cfb137cf
Martha Troubh <indexer@icloud.com>
2019-07-21 08:57:34 -0600

    Activate method for Items Controller

--- app/controllers/dashboard/items_controller.rb
+++ app/controllers/dashboard/items_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Dashboard::ItemsController < Merchant::BaseController
   def index
     @merchant = User.find(current_user.id)
@@ -10,6 +12,14 @@ class Dashboard::ItemsController < Merchant::BaseController
     item.active == false
     item.update(active: false)
     flash[:message] = "#{item.name} is no longer avalible for sale."
+    redirect_to dashboard_items_path
+  end
+
+  def activate
+    item = Item.find(params[:item_id])
+    item.active == true
+    item.update(active: true)
+    flash[:message] = "#{item.name} is now avalible for sale."
     redirect_to dashboard_items_path
   end
 end
