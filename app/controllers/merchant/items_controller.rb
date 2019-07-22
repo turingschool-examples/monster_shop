b99e8d4e7f453b0967fa1e5984c0f1fdb4fd1c24
Martha Troubh <indexer@icloud.com>
2019-07-21 23:58:28 -0600

    New items controller

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -8,4 +8,29 @@ class Merchant::ItemsController < Merchant::BaseController
     flash[:notice] = "#{item.name} has been fulfilled."
     redirect_to merchant_orders_path(Order.find(params[:order_id]))
   end
+
+  def new
+    @item = Item.new(params[:merchant_id])
+  end
+
+  def index
+    @merchant = User.find(current_user.id)
+    @items = Item.all
+  end
+
+  def deactivate
+    item = Item.find(params[:item_id])
+    item.active == false
+    item.update(active: false)
+    flash[:message] = "#{item.name} is no longer avalible for sale."
+    redirect_to dashboard_items_path
+  end
+
+  def activate
+    item = Item.find(params[:item_id])
+    item.active == true
+    item.update(active: true)
+    flash[:message] = "#{item.name} is now avalible for sale."
+    redirect_to dashboard_items_path
+  end
 end
