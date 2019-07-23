4bfcd71804134b0939b22f33954a15d1f8e34499
Jake Miller <jake0miller@gmail.com>
2019-07-23 14:35:26 -0600

    Clean up admin items controller

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,8 @@
 class Admin::ItemsController < Admin::BaseController
   def index
-    # binding.pry
     if params[:id]
       @merchant = Merchant.find(params[:id])
       @items = @merchant.items
-    else
-      @items = Item.all_active
-      @top_items = Item.popular_items(5, 'DESC')
-      @bottom_items = Item.popular_items(5, 'ASC')
     end
   end
 end
