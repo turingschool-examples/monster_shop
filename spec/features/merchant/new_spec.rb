9e839f10e50852ca98d38a966681bc99690854e4
Jake Miller <jake0miller@gmail.com>
2019-07-23 12:39:41 -0600

    Clean up item new test

--- spec/features/merchant/new_spec.rb
+++ spec/features/merchant/new_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'New Merchant Creation' do
   describe 'As a Visitor' do
-    it 'I can link to a new merchant page from merchant index' do
-      visit '/merchants'
+    before :each do
+      visit '/merchants/new'
+    end
 
+    it 'I can link to a new merchant page from merchant index' do
       click_link 'New Merchant'
 
       expect(current_path).to eq('/merchants/new')
     end
 
     it 'I can use the new merchant form to create a new merchant' do
-      visit '/merchants/new'
-
       name = 'Megans Marmalades'
       address = '123 Main St'
       city = "Denver"
@@ -32,8 +32,6 @@ RSpec.describe 'New Merchant Creation' do
     end
 
     it 'I can not create a merchant with an incomplete form' do
-      visit '/merchants/new'
-
       name = 'Megans Marmalades'
 
       fill_in 'Name', with: name
