17b1001cfa75c58b1d0a0397784c1cf232f50d44
Jake Miller <jake0miller@gmail.com>
2019-07-23 10:34:49 -0600

    Delete ship orders method since it is duplicated

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,18 +2,10 @@ class Admin::OrdersController < Admin::BaseController
   def update
     @order = Order.find(params[:id])
     @order.update(status: "shipped")
-
     redirect_to admin_dashboard_path
   end
 
   def index
     @orders = Order.all.sort_by_status
-  end
-
-  def ship_order
-    @order = Order.find([:id])
-    if @order.packaged?
-      @order.status == "shipped"
-    end
   end
 end
