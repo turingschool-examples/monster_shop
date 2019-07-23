a3fe08b796574b7ef0727dcb46a2199d30b6ab46
Jake Miller <jake0miller@gmail.com>
2019-07-23 15:31:45 -0600

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -23,10 +23,10 @@ class Item < ApplicationRecord
   end
   
   def self.popular_items(limit, order = 'DESC')
-    joins(:order_items).
-      select('items.name, order_items.quantity').
-      order("order_items.quantity #{order}").
-      order(:name).limit(limit)
+    joins(:order_items)
+      .select('items.name, order_items.quantity')
+      .order("order_items.quantity #{order}")
+      .order(:name).limit(limit)
   end
 
   def get_order_item(order_id)
