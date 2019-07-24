bb24fde5572f75abaad3bf28ce1229864ad9c6c9
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-24 10:45:11 -0600

    Adjust default status of an order in DB

--- db/migrate/20190718210746_add_column_to_orders.rb
+++ db/migrate/20190718210746_add_column_to_orders.rb
@@ -1,5 +1,5 @@
 class AddColumnToOrders < ActiveRecord::Migration[5.1]
   def change
-    add_column :orders, :status, :integer, default: 0
+    add_column :orders, :status, :integer, default: 1
   end
 end
