3ef66aee7357a6ba45cec5c3da9de0da2a4a1f10
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-24 16:21:46 -0600

    Update test for selecting a merchant when upgrading a user

--- spec/features/admin/user_show_spec.rb
+++ spec/features/admin/user_show_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'Admin' do
       before :each do
         @larry = User.create(name: "Larry Green", address: "345 Blue Lane", city: "Blue City", state: "CA", zip: 56789, email: "green@gmail.com", password: "frogs", role: "admin")
         @user = User.create(name: "Tyler", address: "123 Clarkson St", city: "Denver", state: "CO", zip: 80209, email: "user@gmail.com", password: "cheetos", role: "user")
+        @megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@larry)
         visit admin_user_show_path(@user)
       end
@@ -26,14 +27,22 @@ RSpec.describe 'Admin' do
 
         expect(page).to_not have_content("Password")
         fill_in "Name", with: "Roger Rabbit"
-        select "Merchant", :from => "Role"
+
+        within "#admin_only" do
+          select "employee", :from => "Role"
+          select "Megans Marmalades", :from => 'user[merchant_id]'
+        end
 
         click_on "Update Profile"
 
+        expect(page).to have_content("This user has been updated")
         expect(current_path).to eq(admin_user_show_path(@user))
+
         @user.reload
+
         expect(@user.name).to eq("Roger Rabbit")
-        expect(@user.role).to eq("merchant")
+        expect(@user.role).to eq("employee")
+        expect(@user.merchant_id).to eq(@megan.id)
       end
     end
   end
