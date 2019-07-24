39468517a95353f3eaf6d336d3f44439594f00e5
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-24 16:24:12 -0600

    Remove admin as possible role to be upgrade to

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   validates_presence_of :name, :address, :city, :role
 
   STATES = [ 'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY' ]
-  ROLES = [ 'User', 'Employee', 'Merchant', 'Admin' ]
+  ROLES = [ 'user', 'employee', 'merchant' ]
 
   enum role: ["user", "employee", "merchant", "admin"]
 
