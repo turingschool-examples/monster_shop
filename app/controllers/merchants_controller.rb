ae1bca0d772054185231771fbb42f5f351c91cc5
Jake Miller <jake0miller@gmail.com>
2019-07-24 10:28:05 -0600

    Move edit and udpate to admin controller

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,32 +1,15 @@
 class MerchantsController < ApplicationController
-  before_action :set_merchant, only: [:show, :edit, :update]
-
   def index
     @merchants = Merchant.all
   end
 
   def show
-  end
-
-  def edit
-  end
-
-  def update
-    if @merchant.update_attributes(merchant_params)
-      redirect_to "/merchants/#{@merchant.id}"
-    else
-      generate_flash(@merchant)
-      render :edit
-    end
+    @merchant = Merchant.find(params[:id])
   end
 
   private
 
   def merchant_params
     params.require(:merchant).permit(:name, :address, :city, :state, :zip)
-  end
-
-  def set_merchant
-    @merchant = Merchant.find(params[:id])
   end
 end
