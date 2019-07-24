3da34362a516f9c39a8dc14a7b839399c312b093
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-24 16:25:23 -0600

    Add merchant_id to user update

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -9,5 +9,26 @@ class Admin::UsersController < Admin::BaseController
 
   def edit
     @user = User.find(params[:id])
+    @merchants = Merchant.all
+  end
+
+  def update
+    @user = User.find(params[:id])
+    unless user_params[:merchant_id].scan(/\D/).empty?
+      params[:user][:merchant_id] = Merchant.find_by(name: params[:user][:merchant_id]).id
+    end
+    if @user.update_attributes(user_params)
+      flash[:notice] = "This user has been updated"
+      redirect_to admin_user_show_path(@user)
+    else
+      generate_flash(@user)
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :address, :city, :state, :zip, :email, :role, :merchant_id)
   end
 end
